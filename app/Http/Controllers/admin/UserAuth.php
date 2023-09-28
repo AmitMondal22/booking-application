cb9b92a64a44602f3c89db8c7f3e69c36bc5097f
meettan <meettan@synergicsoftek.com>
2023-09-29 01:37:03 +0530

    first commit

---
+++ app/Http/Controllers/admin/UserAuth.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+
+class UserAuth extends Controller
+{
+    function create_account() : Response {
+        return response()->json("zdmnvcdfm");
+    }
+    function login() : Response {
+        return response()->json("xfbvjfd");
+    }
+}
