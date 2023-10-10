fe63d8ac5c7891aac7f92edfee6e8538378b592c
meettan <meettan@synergicsoftek.com>
2023-10-11 00:41:27 +0530

    drtbfgrge

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
+
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
