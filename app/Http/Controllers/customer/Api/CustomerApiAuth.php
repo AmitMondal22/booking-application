4fe9818960ce2823554d1bbd92f547fa50e03a63
meettan <meettan@synergicsoftek.com>
2023-10-16 21:16:00 +0530

    xv

--- app/Http/Controllers/customer/Api/CustomerApiAuth.php
+++ app/Http/Controllers/customer/Api/CustomerApiAuth.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
-class CustomerApiAuth extends Controller
+class CustomerApiAuth extends ResponceBaseController
 {
     function register(Request $r): JsonResponse
     {
